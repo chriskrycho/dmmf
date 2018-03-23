fbe8d72ae6c41f01dd343e90e62bf7faa3a265fa
Chris Krycho <chris@chriskrycho.com>
2018-03-23 07:44:26 -0600

    More ports from F# to the others.

--- rust/src/domain.rs
+++ rust/src/domain.rs
@@ -48,7 +48,18 @@ pub enum ProductCode {
     Gizmo(GizmoCode),
 }
 
-pub struct UnitQuantity(i32);
+pub struct UnitQuantity(u32);
+
+impl UnitQuantity {
+    pub fn create(qty: u32) -> Result<UnitQuantity, String> {
+        match qty {
+            0 => Err(String::from("`UnitQuantity` cannot be less than 1")),
+            1...1000 => Ok(UnitQuantity(qty)),
+            _ => Err(String::from("`UnitQuantity` cannot be greater than 1000")),
+        }
+    }
+}
+
 pub struct KilogramQuantity(f32);
 
 enum OrderQuantity {
