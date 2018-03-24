9202cd4b2d38b4ca869cf2387ccc0a103562f19a
Chris Krycho <chris@chriskrycho.com>
2018-03-23 20:03:09 -0600

    Finish Rust implementation.

--- rust/src/domain.rs
+++ rust/src/domain.rs
@@ -43,11 +43,13 @@ impl GizmoCode {
     }
 }
 
+#[derive(PartialEq, Debug)]
 pub enum ProductCode {
     Widget(WidgetCode),
     Gizmo(GizmoCode),
 }
 
+#[derive(PartialEq, Debug)]
 pub struct UnitQuantity(u32);
 
 impl UnitQuantity {
@@ -58,10 +60,42 @@ impl UnitQuantity {
             _ => Err(String::from("`UnitQuantity` cannot be greater than 1000")),
         }
     }
+
+    pub fn value(&self) -> u32 {
+        self.0
+    }
+
+    pub fn minimum() -> UnitQuantity {
+        UnitQuantity(1)
+    }
 }
 
+#[derive(PartialEq, Debug)]
 pub struct KilogramQuantity(f32);
 
+impl KilogramQuantity {
+    pub fn create(qty: f32) -> Result<KilogramQuantity, String> {
+        if qty < 0.05 {
+            Err(String::from("`KilogramQuantity` cannot be less than 0.05"))
+        } else if qty > 1000.0 {
+            Err(String::from(
+                "`KilogramQuantity` cannot be more than 1000.00",
+            ))
+        } else {
+            Ok(KilogramQuantity(qty))
+        }
+    }
+
+    pub fn value(&self) -> f32 {
+        self.0
+    }
+
+    pub fn minimum() -> KilogramQuantity {
+        KilogramQuantity(0.05)
+    }
+}
+
+#[derive(PartialEq, Debug)]
 enum OrderQuantity {
     Unit(UnitQuantity),
     Kilogram(KilogramQuantity),
